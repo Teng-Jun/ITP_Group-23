c4389fdb487c40fbaef086f647fd52c0f962b70e
JIAJINSIM <JJSIMYOU@GMAIL.COM>
2024-10-28 06:05:50 +0800

    Combine LLM roberta Model Sentiment Analysis output
    Merge all 3 csv to 1

---
+++ Scrapping Related/JJreddit3rdpart/combine_roberta_sentiment_analysis_result.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+# Define the directory and filenames
+DATA_PATH = r'C:\Users\dclit\OneDrive\Documents\GitHub\ITP_Group-23\Scrapping Related\JJreddit3rdpart'
+csv_files = [
+    os.path.join(DATA_PATH, 'roberta_airdrop_sentiment_results_part1.csv'),
+    os.path.join(DATA_PATH, 'roberta_airdrop_sentiment_results_part2.csv'),
+    os.path.join(DATA_PATH, 'roberta_airdrop_sentiment_results_part3.csv')
+]
+
+# Combine all CSV files into one DataFrame
+combined_df = pd.concat([pd.read_csv(file) for file in csv_files], ignore_index=True)
+
+# Save the combined CSV file
+combined_df.to_csv(os.path.join(DATA_PATH, 'roberta_airdrop_sentiment_combined_results.csv'), index=False)
+
+print("All parts combined successfully!")
