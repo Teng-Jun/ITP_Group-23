ec54ad3bb14d439a464c66f9bb1099f3665b4a51
vanessatan157 <87938172+vanessatan157@users.noreply.github.com>
2024-11-04 12:09:44 +0800

    update header and footer

--- Website Related/AirGuard/urlscan.php
+++ Website Related/AirGuard/urlscan.php
@@ -27,6 +27,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['url']) && isset($_POS
 
 include __DIR__ . '/views/header.php';
 ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>URL Scanner</title>
+        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <link rel="icon" type="image/png" href="image/airguard-favicon-color-32.png">
+    </head>    
+
+
     <h1>URL Scanner</h1>
     <form method="POST" onsubmit="showProgressMessage(event)">
         <label for="url">Enter a URL:</label>
